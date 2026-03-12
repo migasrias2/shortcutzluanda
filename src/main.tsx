34bdadde2e207267da1f84b37f5b1ff6fef03a2a
Miguel Martins <ffawdfa@gmail.com>
2026-03-12 15:34:31 +0100

    Update index.html title and add favicon; modify main.tsx to dynamically set favicon from assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import logoUrl from "../ShortcutzLuanda_Logowhatsapp-removebg-preview.png";
+
+const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]');
+if (favicon) {
+  favicon.href = logoUrl;
+} else {
+  const link = document.createElement("link");
+  link.rel = "icon";
+  link.type = "image/png";
+  link.href = logoUrl;
+  document.head.appendChild(link);
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
